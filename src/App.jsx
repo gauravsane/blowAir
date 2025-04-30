6df300d211c19f3c2e38967ddabfc1aa449a3cfb
gauravsane <sanegaurav360@gmail.com>
2025-04-30 11:15:34 +0530

    chnage

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ const App = () => {
         const intensity =
           lowFreqRange.reduce((a, b) => a + b, 0) / lowFreqRange.length;
 
-        if (intensity > 20 && isHolding) {
+        if (intensity > 60 && isHolding) {
           setLeaves((prev) =>
             prev.map((leaf) => {
               if (leaf.hasMoved || Math.random() > 0.3) return leaf;
